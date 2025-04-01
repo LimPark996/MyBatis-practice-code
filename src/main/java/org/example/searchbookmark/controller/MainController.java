384073fc20262eee26b2700b8ed001e047000ccf
qus0in <qus0in@gmail.com>
2025-04-01 14:49:03 +0900

    검색 완성

--- src/main/java/org/example/searchbookmark/controller/MainController.java
+++ src/main/java/org/example/searchbookmark/controller/MainController.java
@@ -2,15 +2,21 @@ package org.example.searchbookmark.controller;
 
 import org.example.searchbookmark.model.vo.KeywordSearch;
 import org.example.searchbookmark.service.SearchService;
+import org.example.searchbookmark.util.MyLogger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 @Controller
 //@RequestMapping("/") // 이런 경우는 생략해도 무방함
 public class MainController {
+    final private MyLogger logger = new MyLogger(this.getClass().getSimpleName());
+
     // 멤버변수
     private final SearchService searchService;
     // 구별을 굳이 안해도 돼 -> 1:1 대응이 되니까
@@ -21,8 +27,18 @@ public class MainController {
     }
 
     @GetMapping
-    public String index(Model model) throws Exception {
-        List<KeywordSearch> result = searchService.searchByKeyword("4월은 너의 거짓말");
+    public String index(Model model,
+//                        @RequestParam("keyword") String keyword
+                        // required = true 가 숨겨져 있어
+                        @RequestParam(value = "keyword",
+//                                defaultValue = "야구 순위",
+                                required = false) String keyword
+    ) throws Exception {
+        logger.info(keyword);
+        if (keyword == null) {
+            return "index";
+        }
+        List<KeywordSearch> result = searchService.searchByKeyword(keyword);
         model.addAttribute("result", result);
         return "index";
     }
