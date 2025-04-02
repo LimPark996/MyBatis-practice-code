850feb3722d4d2af2ebabe1e1a78655dd6fcb96f
유미 <uji835311@gmail.com>
2025-04-02 11:19:39 +0900

    주석 추가

--- src/main/java/org/example/searchbookmark/util/ObjectMapperMixin.java
+++ src/main/java/org/example/searchbookmark/util/ObjectMapperMixin.java
@@ -5,3 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public interface ObjectMapperMixin {
     ObjectMapper objectMapper = new ObjectMapper();
 }
+
+// 이 ObjectMapperMixin은
+// ObjectMapper를 static 상수로 미리 만들어두고,
+// 여러 클래스에서 재사용하기 위한 인터페이스예요.
