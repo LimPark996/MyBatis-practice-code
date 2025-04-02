b4c1f900a33c98b17d028cf15f26b6d86d1fb82f
유미 <uji835311@gmail.com>
2025-04-02 11:20:37 +0900

    주석 추가

--- src/main/java/org/example/searchbookmark/util/ObjectMapperMixin.java
+++ src/main/java/org/example/searchbookmark/util/ObjectMapperMixin.java
@@ -9,3 +9,9 @@ public interface ObjectMapperMixin {
 // 이 ObjectMapperMixin은
 // ObjectMapper를 static 상수로 미리 만들어두고,
 // 여러 클래스에서 재사용하기 위한 인터페이스예요.
+
+// ObjectMapper는 Jackson 라이브러리에서 제공하는 클래스로,
+// Java 객체를 JSON으로 바꾸거나(JSON 직렬화),
+// JSON 문자열을 Java 객체로 바꾸는 것(JSON 역직렬화) 을 도와주는 도구예요.
+
+// Jackson 라이브러리는 스프링에서 기본으로 들어 있는 JSON 처리 도구예요.
